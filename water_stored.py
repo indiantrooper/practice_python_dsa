1a775a4abdbb3075c18bbdaa6864363cda08d778
indiantrooper <amrit.iitkgp@gmail.com>
2023-09-22 16:35:40 +0530

    Water problem array

--- water_stored.py
+++ water_stored.py
@@ -2,12 +2,12 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 
-# Trapping rain water array problem. 
-rain = [3,0,2,0,4]
-n = len(rain)
+# Trapping height water array problem. 
+height = [3,0,2,0,4]
+n = len(height)
 #plot how the containers will look like
 X_axis = np.arange(n))
-plt.bar(X_axis,rain,width=1)
+plt.bar(X_axis,height,width=1)
 plt.show()
 
 # Logic: If we calculate water[i] for i in index (array); water[i]= -array[i] + min(Left[i],right[i])
@@ -15,19 +15,18 @@ plt.show()
 
 water = 0
 left =[0]*n
-left[1]=rain[0]
+left[1]=height[0]
 right = [0]*n
-right[n-1]=0
 
-for i in range(2,n):
-    left[i]=max(left[i-1],rain[i-1])
+for i in range(1,n-1):
+    left[i+1]=max(left[i],height[i])
 
 for j in range(n-2,0,-1):
-    right[j]=max(right[j+1],rain[j+1])
+    right[j]=max(right[j+1],height[j+1])
 
 for i in range(0,n):
-    if(min(left[i],right[i])-rain[i]>0):
-        water+=min(left[i],right[i])-rain[i]
+    if(min(left[i],right[i])-height[i]>0):
+        water+=min(left[i],right[i])-height[i]
         print(water)
 
 print("Total water stored =", water,"units")
